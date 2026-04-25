3d4295ffffc32a27fc7bc71373425bcf5c3d057f
MichaelofNebadon <michaeldrowley@gmail.com>
2026-04-24 20:01:21 -0700

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,8 @@
+# THE UNIFIED HOOK: Bringing the four lineages under one name
+from .dynamics import nppsc_field_step
+from .lyapunov import estimate_lyapunov
+from .measures import compute_invariant_measure
+from .separatrix import identify_separatrix
+
+__version__ = "1.0.0"
+print("DA'AT UNIFIED: nppsc package activated.")
