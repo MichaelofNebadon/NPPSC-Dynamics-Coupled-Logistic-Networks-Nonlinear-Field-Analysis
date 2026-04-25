a71faf1c41a8cbd571da964443c092024a23b430
MichaelofNebadon <michaeldrowley@gmail.com>
2026-04-24 19:46:58 -0700

    Initialize nppsc module with core imports

---
+++ nppsc/__init__.py
@@ -0,0 +1,7 @@
+# nppsc/__init__.py
+# Bridging the Four Lineages into a single Module
+
+from .dynamics import nppsc_field_step
+from .lyapunov import estimate_lyapunov
+from .measures import compute_invariant_measure
+from .separatrix import identify_separatrix
